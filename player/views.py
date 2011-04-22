d57f40a6fa64dd3f8f577d04664f7143c865f190
zeratul2099# <zeratul2099@googlemail.com#>
2011-04-23 00:31:46 +0200

    initial revision

---
+++ player/views.py
@@ -0,0 +1,24 @@
+# Create your views here.
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render_to_response
+import os, json
+from django.conf import settings
+def home(request):
+
+  return render_to_response("mainview.html",{});
+  
+def getFiles(request):
+  rootPath = settings.MUSIC_ROOT
+  try:
+	if request.method == 'POST' and "path" in request.POST:
+	  
+	  path = request.POST["path"].lstrip("/");
+	  print os.listdir(os.path.join(rootPath, path))
+	  files = sorted(map(lambda x: os.path.join(path,x), filter(lambda x: x.endswith(".ogg"),os.listdir(os.path.join(rootPath, path)))));
+	  dirs = filter(lambda x: os.path.isdir(os.path.join(rootPath, path, x)),os.listdir(os.path.join(rootPath, path)))
+	else:
+	  dirs = filter(lambda x: os.path.isdir(os.path.join(rootPath, x)),os.listdir(rootPath))
+	  files = sorted(filter(lambda x: x.endswith(".ogg"),os.listdir("music")));
+	return HttpResponse(json.dumps([files,dirs]), mimetype="application/json")
+  except Exception, e:
+	print e
---
+++ urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+     url(r'^$', 'html5musicplay.player.views.home', name='home'),
+     url(r'^getFiles$', 'html5musicplay.player.views.getFiles'),
+     (r'^music/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MUSIC_ROOT}),
+     (r'^css/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.CSS_ROOT}),
+    # url(r'^html5musicplay/', include('html5musicplay.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
