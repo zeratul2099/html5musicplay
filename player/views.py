9b60589622b75fbb92d21b412a34ea2b369ea035
zeratul2099# <zeratul2099@googlemail.com#>
2011-04-29 15:33:24 +0200

    reading and displaying metatags of ogg-files

--- player/views.py
+++ player/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 import os, json
 from django.conf import settings
+import ogg.vorbis
+
 def home(request):
 
   return render_to_response("mainview.html",{});
@@ -13,20 +15,21 @@ def getFiles(request):
 	if request.method == 'POST' and "path" in request.POST:
 	  
 	  path = request.POST["path"].lstrip("/");
-	  print os.listdir(os.path.join(rootPath, path))
 	  files = sorted(map(lambda x: os.path.join(path,x), filter(lambda x: x.endswith(".ogg"),os.listdir(os.path.join(rootPath, path)))));
 	  dirs = filter(lambda x: os.path.isdir(os.path.join(rootPath, path, x)),os.listdir(os.path.join(rootPath, path)))
 	else:
 	  dirs = filter(lambda x: os.path.isdir(os.path.join(rootPath, x)),os.listdir(rootPath))
 	  files = sorted(filter(lambda x: x.endswith(".ogg"),os.listdir("music")));
-	return HttpResponse(json.dumps([files,dirs]), mimetype="application/json")
+	fileData = {}
+	for i, fileName in enumerate(files):
+	  fileData[i] = (fileName,ogg.vorbis.VorbisFile(os.path.join(rootPath, fileName)).comment().as_dict())
+	return HttpResponse(json.dumps([fileData,dirs]), mimetype="application/json")
   except Exception, e:
 	print e
 	
 	
 # TODO:
 # volume control
-# shuffle, repeat
 # style player
 # seeking
 # show id-tag information
